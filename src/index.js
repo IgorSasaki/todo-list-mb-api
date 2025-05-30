612a6c08cdb3bd46b37e51a7c8dfc10d317b04b5
IgorSasaki <igor-sasaki@hotmail.com>
2025-05-30 10:16:34 -0300

    feat (index): Integrate routes and database config

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 const express = require("express");
+const { AppDataSource } = require("./config/data-source");
+const taskRoutes = require("./routes/task.routes");
+
+require("reflect-metadata");
 
 const app = express();
 const PORT = 3333;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API rodando!");
-});
+AppDataSource.initialize()
+  .then(() => {
+    console.log("📦 Banco de dados conectado!");
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
+    app.use("/", taskRoutes);
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Erro ao conectar com o banco:", err);
+  });
